179901b50078e1a894b0c5bfa1b521c109bc09a2
Nihal Cntrlx <43931373+CntrlX@users.noreply.github.com>
2025-05-24 22:01:34 +0530

    Update package version to 0.1.3, enhance README with PDF merging capabilities, and implement merge functionality in PdfKit node using pdf-merger-js.

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 
 # n8n-nodes-pdfkit
 
-Use this node to convert images to PDF. At the moment only one option available to comnvert all images on a single item to a pdf. (will do this for each item)
+This node provides PDF manipulation capabilities using PDFKit. The following operations are available:
+
+## Convert Images to PDF
+Convert all images on a single item to a PDF. This will be done for each item in the workflow.
 ![image](https://github.com/bramkn/n8n-nodes-pdfkit/assets/74856333/b18cf61e-f1fb-4165-859f-c95a01ecb982)
 
+## Merge PDFs
+Merge two PDF documents into a single PDF. Specify the binary fields containing the PDFs to merge and configure the output.
 
 ## License
 
--- nodes/PdfKit/PdfKit.node.ts
+++ nodes/PdfKit/PdfKit.node.ts
@@ -8,6 +8,7 @@ import {
 
 const PDFDocument = require('pdfkit');
 var sizeOf = require('image-size');
+const PDFMerger = require('pdf-merger-js');
 
 export class PdfKit implements INodeType {
 	description: INodeTypeDescription = {
@@ -31,11 +32,16 @@ export class PdfKit implements INodeType {
 				required: true,
 				options:[
 					{
-						name:'Covert Images To PDF',
+						name:'Convert Images To PDF',
 						value:'imagesToPDF'
+					},
+					{
+						name:'Merge PDFs',
+						value:'mergePDFs'
 					}
 				],
 			},
+			// Fields for imagesToPDF operation
 			{
 				displayName: 'Destination Key',
 				name: 'outputKey',
@@ -43,6 +49,14 @@ export class PdfKit implements INodeType {
 				default: 'data',
 				required: true,
 				description: 'The name the binary key to copy data to',
+				displayOptions: {
+					show: {
+						operation: [
+							'imagesToPDF',
+							'mergePDFs',
+						],
+					},
+				},
 			},
 			{
 				displayName: 'PDF Name',
@@ -51,6 +65,14 @@ export class PdfKit implements INodeType {
 				default: '',
 				required: true,
 				description: 'The name of the output PDF',
+				displayOptions: {
+					show: {
+						operation: [
+							'imagesToPDF',
+							'mergePDFs',
+						],
+					},
+				},
 			},
 			{
 				displayName: 'Keep Images',
@@ -58,15 +80,67 @@ export class PdfKit implements INodeType {
 				type: 'boolean',
 				default: false,
 				description: 'Whether to keep images that were used in the PDF',
+				displayOptions: {
+					show: {
+						operation: [
+							'imagesToPDF',
+						],
+					},
+				},
+			},
+			// Fields for mergePDFs operation
+			{
+				displayName: 'First PDF Binary Field',
+				name: 'firstPdfField',
+				type: 'string',
+				default: '',
+				required: true,
+				description: 'The binary field containing the first PDF to merge',
+				displayOptions: {
+					show: {
+						operation: [
+							'mergePDFs',
+						],
+					},
+				},
+			},
+			{
+				displayName: 'Second PDF Binary Field',
+				name: 'secondPdfField',
+				type: 'string',
+				default: '',
+				required: true,
+				description: 'The binary field containing the second PDF to merge',
+				displayOptions: {
+					show: {
+						operation: [
+							'mergePDFs',
+						],
+					},
+				},
+			},
+			{
+				displayName: 'Keep Source PDFs',
+				name: 'keepSourcePdfs',
+				type: 'boolean',
+				default: false,
+				description: 'Whether to keep the source PDFs in the binary data',
+				displayOptions: {
+					show: {
+						operation: [
+							'mergePDFs',
+						],
+					},
+				},
 			},
 		],
 	};
 
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 		const items = this.getInputData();
-
+		const operation = this.getNodeParameter('operation', 0) as string;
+		
 		let item: INodeExecutionData;
-		const keepImages = this.getNodeParameter('keepImages', 0, false) as boolean;
 
 		for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
 			try {
@@ -78,27 +152,75 @@ export class PdfKit implements INodeType {
 					throw new NodeOperationError(this.getNode(), 'No binary data exists on item!');
 				}
 
-				let doc;
-				for (var [index,key] of Object.keys(item.binary).entries()){
-					const binary = Object.assign({},item.binary[key]);
-					if(binary.fileType==='image'){
-						const binaryDataBuffer = await this.helpers.getBinaryDataBuffer(itemIndex, key);
-						const dimensions = sizeOf(binaryDataBuffer);
-						const size =[dimensions.width,dimensions.height];
-						if(index !== 0){
-							doc.addPage({size:size});
-						}
-						else{
-							doc = new PDFDocument({margin:0, size:size});
-						}
-						doc.image(binaryDataBuffer, 0, 0, { fit: size, align: 'center', valign: 'center' })
-						if(!keepImages){
-							delete item.binary[key];
+				// Handle different operations
+				if (operation === 'imagesToPDF') {
+					// Original imagesToPDF operation
+					const keepImages = this.getNodeParameter('keepImages', itemIndex, false) as boolean;
+					
+					let doc;
+					for (var [index,key] of Object.keys(item.binary).entries()){
+						const binary = Object.assign({},item.binary[key]);
+						if(binary.fileType==='image'){
+							const binaryDataBuffer = await this.helpers.getBinaryDataBuffer(itemIndex, key);
+							const dimensions = sizeOf(binaryDataBuffer);
+							const size =[dimensions.width,dimensions.height];
+							if(index !== 0){
+								doc.addPage({size:size});
+							}
+							else{
+								doc = new PDFDocument({margin:0, size:size});
+							}
+							doc.image(binaryDataBuffer, 0, 0, { fit: size, align: 'center', valign: 'center' })
+							if(!keepImages){
+								delete item.binary[key];
+							}
 						}
 					}
+					doc.end();
+					item.binary![outputKey] = await this.helpers.prepareBinaryData(doc, `${outputName}.pdf`);
+				
+				} else if (operation === 'mergePDFs') {
+					// New mergePDFs operation
+					const firstPdfField = this.getNodeParameter('firstPdfField', itemIndex, '') as string;
+					const secondPdfField = this.getNodeParameter('secondPdfField', itemIndex, '') as string;
+					const keepSourcePdfs = this.getNodeParameter('keepSourcePdfs', itemIndex, false) as boolean;
+					
+					// Check if both PDF fields exist in binary data
+					if (!item.binary[firstPdfField]) {
+						throw new NodeOperationError(this.getNode(), `First PDF field "${firstPdfField}" does not exist on item!`);
+					}
+					
+					if (!item.binary[secondPdfField]) {
+						throw new NodeOperationError(this.getNode(), `Second PDF field "${secondPdfField}" does not exist on item!`);
+					}
+					
+					// Get binary buffers for both PDFs
+					const firstPdfBuffer = await this.helpers.getBinaryDataBuffer(itemIndex, firstPdfField);
+					const secondPdfBuffer = await this.helpers.getBinaryDataBuffer(itemIndex, secondPdfField);
+					
+					// Create a new PDF merger
+					const merger = new PDFMerger();
+					
+					// Add both PDFs to the merger
+					await merger.add(firstPdfBuffer);
+					await merger.add(secondPdfBuffer);
+					
+					// Get the merged PDF as a buffer
+					const mergedPdfBuffer = await merger.saveAsBuffer();
+					
+					// Save the merged PDF to the binary data
+					item.binary![outputKey] = await this.helpers.prepareBinaryData(
+						mergedPdfBuffer,
+						`${outputName}.pdf`,
+						'application/pdf'
+					);
+					
+					// Optionally remove source PDFs
+					if (!keepSourcePdfs) {
+						delete item.binary[firstPdfField];
+						delete item.binary[secondPdfField];
+					}
 				}
-				doc.end();
-				item.binary![outputKey] = await this.helpers.prepareBinaryData(doc, `${outputName}.pdf`);
 
 			} catch (error) {
 				// This node should never fail but we want to showcase how
